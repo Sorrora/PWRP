5f6f1aef1fadc0330650aee3644ab6c434cbaa1b
Prestige Worldwide <prestigeworldwideroleplay@gmail.com>
2017-09-06 11:01:28 -0700

    added two additional police stations to esx_policejob

--- resources/[esx]/esx_policejob/config.lua
+++ resources/[esx]/esx_policejob/config.lua
@@ -38,6 +38,7 @@ Config.PoliceStations = {
 			{name = 'sheriff3', label = 'Sheriff Crown Victoria'},
 			{name = 'police', label = 'Unmarked Crown Victoria'},
 			{name = 'policeb', label = 'Motorcycle'},
+			{name = 'police3', label = 'Super Fucky Mustang'},
 		},
 
 		Cloakrooms = {
@@ -73,6 +74,112 @@ Config.PoliceStations = {
 			{x = 448.417, y = -973.208, z = 29.689}
 		}
 
-	}
+	},
+	
+	SandyShores = {
 
+		Blip = {
+			Pos   = {x = 1857.6206054688, y = 3679.0146484375, z = 33.026447296143},
+			Color = 29
+		},
+
+		AuthorizedWeapons = {
+			{name = 'WEAPON_NIGHTSTICK',       price = 200},
+			{name = 'WEAPON_COMBATPISTOL',     price = 300},
+			{name = 'WEAPON_ASSAULTSMG',       price = 1250},
+			{name = 'WEAPON_CARBINERIFLE',     price = 1500},
+			{name = 'WEAPON_PUMPSHOTGUN',      price = 600},
+			{name = 'WEAPON_STUNGUN',          price = 500},
+			{name = 'WEAPON_FLASHLIGHT',       price = 80},
+			{name = 'WEAPON_FIREEXTINGUISHER', price = 120},
+			{name = 'WEAPON_FLAREGUN',         price = 60},
+			{name = 'GADGET_PARACHUTE',        price = 300},
+		},
+
+		AuthorizedVehicles = {
+			{name = 'police2' , label = 'City Police Charger'},
+			{name = 'police4', label = 'City Police Interceptor'},
+			{name = 'sheriff', label = 'Sheriff Charger'},
+			{name = 'sheriff2', label = 'Sheriff SUV'},
+			{name = 'sheriff3', label = 'Sheriff Crown Victoria'},
+			{name = 'police', label = 'Unmarked Crown Victoria'},
+			{name = 'policeb', label = 'Motorcycle'},
+			{name = 'police3', label = 'Super Fucky Mustang'},
+		},
+
+		Cloakrooms = {
+			{x = 1857.3419189453, y = 3689.0759277344, z = 33.267078399658}
+		},
+
+		Armories = {
+			{x = 1848.7974853516, y = 3689.8078613281, z = 33.267078399658}
+		},
+
+		Vehicles = {
+			{
+				Spawner    = {x = 1862.3001708984, y = 3682.5014648438, z = 32.768924713135},
+				SpawnPoint = {x = 1868.5006103516, y = 3681.73046875, z = 33.626258850098},
+				Heading    = 90.0
+			}
+		},
+		Helicopters = {},
+
+		VehicleDeleters = {},
+
+		BossActions = {}
+	},
+	
+	PaletoBay = {
+
+		Blip = {
+			Pos   = {x = -439.64807128906, y = 6019.7138671875, z = 30.773178100586},
+			Color = 29
+		},
+
+		AuthorizedWeapons = {
+			{name = 'WEAPON_NIGHTSTICK',       price = 200},
+			{name = 'WEAPON_COMBATPISTOL',     price = 300},
+			{name = 'WEAPON_ASSAULTSMG',       price = 1250},
+			{name = 'WEAPON_CARBINERIFLE',     price = 1500},
+			{name = 'WEAPON_PUMPSHOTGUN',      price = 600},
+			{name = 'WEAPON_STUNGUN',          price = 500},
+			{name = 'WEAPON_FLASHLIGHT',       price = 80},
+			{name = 'WEAPON_FIREEXTINGUISHER', price = 120},
+			{name = 'WEAPON_FLAREGUN',         price = 60},
+			{name = 'GADGET_PARACHUTE',        price = 300},
+		},
+
+		AuthorizedVehicles = {
+			{name = 'police2' , label = 'City Police Charger'},
+			{name = 'police4', label = 'City Police Interceptor'},
+			{name = 'sheriff', label = 'Sheriff Charger'},
+			{name = 'sheriff2', label = 'Sheriff SUV'},
+			{name = 'sheriff3', label = 'Sheriff Crown Victoria'},
+			{name = 'police', label = 'Unmarked Crown Victoria'},
+			{name = 'policeb', label = 'Motorcycle'},
+			{name = 'police3', label = 'Super Fucky Mustang'},
+		},
+
+		Cloakrooms = {
+			{x = -449.71078491211, y = 6016.5395507813, z = 30.71639251709}
+		},
+
+		Armories = {
+			{x = -448.12261962891, y = 6008.5986328125, z = 30.71639251709}
+		},
+
+		Vehicles = {
+			{
+				Spawner    = {x = -460.00796508789, y = 6015.8295898438, z = 30.490106582642},
+				SpawnPoint = {x = -479.51629638672, y = 6027.7080078125, z = 31.340547561646},
+				Heading    = 90.0
+			}
+		},
+		
+		Helicopters = {},
+
+		VehicleDeleters = {},
+
+		BossActions = {}
+	}
 }
